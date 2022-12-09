39bc1488f10667883991949bc7ced0b91709e375
Abhishek Singh <91690267+isinghabhishek@users.noreply.github.com>
2022-12-09 15:13:36 +0530

    Update Avg.java

--- 02 Variables & Data Types/Avg.java
+++ 02 Variables & Data Types/Avg.java
@@ -2,11 +2,14 @@ import java.util.*;
 
 public class Avg {
     public static void main(String[] args) {
+        // taking input 
         Scanner sc = new Scanner(System.in);
         int A = sc.nextInt();
         int B = sc.nextInt();
         int C = sc.nextInt();
+        // calculating average
         int average = (A + B + C)/3;
+        // printing average
         System.out.println("Average of the Given Numbers:" + average);
     }
 }
