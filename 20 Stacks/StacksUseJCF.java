b6e7cf193fc6c7570f492ebabe1a73da61c38323
isinghabhishek <singhabhishek3006.abhi@gmail.com>
2023-02-14 14:11:40 +0530

    update: stack

---
+++ 20 Stacks/StacksUseJCF.java
@@ -0,0 +1,19 @@
+
+// java collection framework(JCF)
+import java.util.*;
+public class StacksUseJCF {
+    
+    public static void main(String[] args) {
+        
+        Stack<Integer> s = new Stack<>();
+
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        while(!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
