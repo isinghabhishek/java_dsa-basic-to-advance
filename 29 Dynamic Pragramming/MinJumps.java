3ba58d174ca1af3b52f8603f6b6fa94b87f49e52
isinghabhishek <singhabhishek3006.abhi@gmail.com>
2023-07-30 16:38:56 +0530

    add: files

---
+++ 29 Dynamic Pragramming/MinJumps.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class MinJumps {
+    public static int minJump(int nums[]) {
+        int n = nums.length; 
+        int dp[] = new int[n+1];
+        Arrays.fill(dp, -1);
+        dp[n-1] = 0;
+
+        for(int i=n-2; i>=0; i--) {
+            int steps = nums[i];
+            int ans = Integer.MAX_VALUE;
+            for(int j=i+1; j<=i+steps && j<n; j++) {
+                if(dp[j] != -1) {
+                    ans = Math.min(ans, dp[j]+1);
+                }
+            }
+            if(ans != Integer.MAX_VALUE) {
+                dp[i] = ans;
+            }
+        }
+        return dp[0];
+    }
+    public static void main(String[] args) {
+        int nums[] = {2, 3, 1, 1, 4};
+        System.out.println(minJump(nums));
+    }
+}
